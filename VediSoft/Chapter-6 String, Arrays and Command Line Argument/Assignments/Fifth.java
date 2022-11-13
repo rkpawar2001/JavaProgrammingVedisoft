5b8e06b2081b729ec61fc056540864d9702835f8
rkpawar2001 <rkpawar2001@gmail.com>
2022-11-13 13:52:20 +0530

    Array Assignment

--- VediSoft/Chapter-6 String, Arrays and Command Line Argument/Assignments/Fifth.java
+++ VediSoft/Chapter-6 String, Arrays and Command Line Argument/Assignments/Fifth.java
@@ -7,19 +7,15 @@ import java.util.Scanner;
 
 class Fifth {
     public static void main(String[] args) {
-        int num[] = new int[10];
-        int i, sum = 0, avg = 0;
+        String name[] = new String[10];
+        int i;
 
         Scanner in = new Scanner(System.in);
-        for (i = 0; i < num.length; i++) {
-            System.out.println("Enter Ten Names " + (i + 1) + ":");
-            num[i] = in.nextInt();
-            sum += num[i];
+        for (i = 0; i < name.length; i++) {
+            System.out.println("Enter Name " + (i + 1) + ":");
+            name[i] = in.nextLine();
         }
 
-        avg = sum / num.length;
-
-        System.out.println("Sum : " + sum);
-        System.out.println("Average : " + avg);
+        System.out.println("Names that Starts with A : " + name[]);
     }
 }
